21af6c36baa634a8374c6f1057abcd08158caef4
Matt Parmett <1214074+mattparmett@users.noreply.github.com>
2023-08-19 16:42:09 -0700

    Refactor and add original/refactored test outputs.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -4,36 +4,40 @@ class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
+        self.max_quality = 50
+        self.min_quality = 0
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+
+            item.sell_in -= 1
+            quality_change = -1  # by default quality decr by 1 each day
+
+            if item.name == "Aged Brie":
+                quality_change = 1
+
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in <= 0:
+                    quality_change = -item.quality
+                elif item.sell_in <= 5:
+                    quality_change = 3
+                elif item.sell_in <= 10:
+                    quality_change = 2
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    quality_change = 1
+
+            elif "Conjured" in item.name:
+                quality_change = -2
+
+            item.quality += quality_change
+
+            if item.quality > self.max_quality:
+                item.quality = self.max_quality
+
+            if item.quality < self.min_quality:
+                item.quality = self.min_quality
 
 
 class Item:
